6c28128b9572c6f59cf26405d00f9adeaa3c3e75
nshrestha <nshrestha@msn.com>
2020-05-09 13:06:57 -0500

    create payment event

---
+++ src/main/java/guru/springframework/msscssm/domain/PaymentEvent.java
@@ -0,0 +1,5 @@
+package guru.springframework.msscssm.domain;
+
+public enum PaymentEvent {
+    PRE_AUTHORIZE, PRE_AUTHORIZE_APPROVED, PRE_AUTHORIZE_DECLINED, AUTHORIZED, AUTH_APPROVED, AUTH_DECLINED
+}
